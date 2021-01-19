e4ab73e0ba10bfd0f68ca7c122c76421fed9c318
kouharakouki <koki.kohara916@gmail.com>
2021-01-19 10:38:57 +0000

    [Finish]4-10

---
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '2228243927e239a9d3c2c504e56fcaff78bb5bead8b85a9d1ca24cd2682a9bf7d96c687eb162f66149e306a48e7d053f9954824d899dc328e5ddf8af774b80a5'
