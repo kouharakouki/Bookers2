1955b49b405c5b32919b666625b077525b8dba79
kouharakouki <koki.kohara916@gmail.com>
2021-02-07 08:47:54 +0000

    [Finish] application of bookers2-7

--- bookers2/app/assets/javascripts/users.coffee
+++ bookers2/app/assets/javascripts/users.coffee
@@ -1,3 +1,14 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$ ->
+  $("#address_zipcode").jpostal({
+    postcode : [ "#address_zipcode" ],
+
+    address  : {
+      "#user_prefecture_code" : "%3", # 都道府県が入力される
+      "#address_city"            : "%4%5", # 市区町村と町域が入力される
+      "#address_street"          : "%6%7" # 大口事務所の番地と名称が入力される
+                }
+  })
--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -24,6 +24,17 @@ class User < ApplicationRecord
   def following?(user)
     following_users.include?(user)
   end
+  
+  include JpPrefecture
+  jp_prefecture :prefecture_code
+  
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end
 
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
