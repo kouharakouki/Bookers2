1ad6dea3ba0ddc1e3781b1c2d48b84e23b0466ab
kouharakouki <koki.kohara916@gmail.com>
2021-02-07 06:16:59 +0000

    [Finish] application of bookers2-6

--- bookers2/app/controllers/book_comments_controller.rb
+++ bookers2/app/controllers/book_comments_controller.rb
@@ -1,20 +1,23 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
-      redirect_to book_path(@book)
+      # redirect_to book_path(@book)
     else
       @book_new = Book.new
-      render "books/show"
     end
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    # BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
